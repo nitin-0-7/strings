d26a9386def4a17d07fd9a2c2774575a6a957c11
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-22 11:31:06 +0530

    print string

---
+++ print string .c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   char arr[]="Nitin";
+   int i=0;
+   while(i<5){
+       printf("%c ",arr[i]);
+   
+    i++;
+   }
+       
+    return 0;
+}
