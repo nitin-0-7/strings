f98e7f5323db6a4a037797264b75cf97f0cb4117
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-22 16:08:54 +0530

    return the length of the string

---
+++ return the length of the string.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char* str="Nitin chauhan";
+    int x=strlen(str);
+    printf("%d",x);
+
+    return 0;
+}
